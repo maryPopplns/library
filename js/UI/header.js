22b6a031c92e29c9e6a6cf99b9005183787e3b56
Spencer Knight <spencer.knightusn@gmail.com>
2021-06-25 09:19:58 -0700

    created header

---
+++ js/UI/header.js
@@ -0,0 +1,15 @@
+function header() {
+  const HEADER = document.createElement("H1");
+  const HORIZONTAL_RULE = document.createElement("hr");
+  const BODY = document.querySelector("body");
+  const APP = document.querySelector("#app");
+
+  HEADER.setAttribute("id", "header");
+  HORIZONTAL_RULE.setAttribute("id", "horizontal_rule");
+  HEADER.textContent = "Reading List";
+
+  BODY.insertBefore(HEADER, APP);
+  HEADER.append(HORIZONTAL_RULE);
+}
+
+export { header };
